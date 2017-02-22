eb07364029088c32e395fa2e3c62c4b5540a20be
regenalgrant <regenal@mac.com>
2017-02-21 20:02:40 -0800

    html struct.

--- src/concurrency_server.py
+++ src/concurrency_server.py
@@ -5,3 +5,16 @@ import email.utils
 import mimetypes
 import io
 import os
+
+
+def build_file_structre_html(directory):
+    print(directory)
+    body = "<!DOCTYPE html>\r\n<html>\r\n<ul>\r\n<body>\r\n<h1>Code Fellows</h1>\r\n"
+    for item in os.listdir(directory):
+        print(item)
+        if os.path.isdir(os.path.join(directory, item)):
+            body += "<li><a href=\"{}/\">{}</a></li>\r\n".format(item, item)
+        else:
+            body += "<li><a href=\"{}\">{}</a></li>\r\n".format(item, item)
+    body += "</ul>\r\n</body>\r\n</html>"
+    return body
